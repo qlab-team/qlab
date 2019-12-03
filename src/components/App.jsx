e71f485949325d72b242a371d9b74201954401c8
sidiousvic <victor6rene@gmail.com>
2019-12-03 21:11:04 +0900

    Added Dashboard, implemented Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,59 @@
 import React from "react";
 import "../style/App.css";
-import QuizMain from './QuizComponents/QuizMain'
+
+// components
+import Dashboard from "./Dashboard/Dashboard";
+
+// material UI
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <QuizMain /> 
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              QLAB
+            </Typography>
+            <a href="/quiz">
+              <Button color="inherit">Login</Button>
+            </a>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/quiz" component={Dashboard}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
