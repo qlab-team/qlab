ded140ddf7c7ded7abe0ba47ee355c96d39fb982
Derek <derek.forbes77@gmail.com>
2019-12-05 08:06:17 +0900

    small change to quizActions

--- src/store/actions/quizActions.js
+++ src/store/actions/quizActions.js
@@ -5,4 +5,22 @@ const updateQuizInfo = quizId => {
     }  
   };
 
-  export default updateQuizInfo
+//   export const getQuiz = quizId => {
+//     return (dispatch, getState, { getFirestore }) => {
+//       // make async call to database
+//       const firestore = getFirestore();
+//       firestore
+//         .collection("Quizzes")
+//         .add({
+//           ...user,
+//           username: "eriko",
+//           points: 2,
+//           createdAt: new Date()
+//         })
+//         .then(() => {
+//           dispatch({ type: "USER_LOGGED_IN" });
+//         });
+//     };
+//   };
+
+export default updateQuizInfo
