d0d8e1bbe2e45dc63c9a2166d3ad74bfba88fc66
sidiousvic <victor6rene@gmail.com>
2019-12-05 16:16:37 +0900

    Update QuizCard.jsx

--- src/components/Dashboard/Quizzes/QuizCard.jsx
+++ src/components/Dashboard/Quizzes/QuizCard.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 // components
 import Title from "../Title";
 // material ui
-import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import { connect } from "react-redux";
 // actions
