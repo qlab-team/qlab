40522c7f02fb1ea57ffe399f4ddea11a973f52c5
sidiousvic <victor6rene@gmail.com>
2019-12-08 17:59:22 +0900

    Updated Copyright function

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -28,7 +28,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        QLAB
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
