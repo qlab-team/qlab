5287ff9ca29b3b745710b8820d32b8a795fcbdc1
sidiousvic <victor6rene@gmail.com>
2019-12-09 17:07:25 +0900

    prettier

--- src/components/Quiz/QuizAnswer.jsx
+++ src/components/Quiz/QuizAnswer.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
+import React from "react";
 import { Grid, Paper, withStyles } from "@material-ui/core";
 
 const styles = {
-    Paper: {
-      padding: 20,
-      marginTop: 10,
-      marginBottom: 10,
-      height: 60,
-      backgroundColor: "white",
-      '&:hover': {
-          backgroundColor: 'blue !important'
-       }
-    },
-
-    selected: {
-        backgroundColor: "blue !important"
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 60,
+    backgroundColor: "white",
+    "&:hover": {
+      backgroundColor: "blue !important"
     }
-  };
+  },
 
-const colorChanger = (event) => {
-    console.log('clicked')
-}
+  selected: {
+    backgroundColor: "blue !important"
+  }
+};
 
+const colorChanger = event => {
+  console.log("clicked");
+};
 
 function QuizAnswer(props) {
-    // const [style, setStyle]= useState(styles)
-   
-    return(
-        <Grid item xs={6} onClick={ (event) => { colorChanger(event); props.getCurrentAnswer(props.answer, props.correctAnswer);}}>
-          <Paper className={props.classes.Paper}>{props.answer}</Paper>
-        </Grid>
-    )
+  return (
+    <Grid
+      item
+      xs={6}
+      onClick={event => {
+        colorChanger(event);
+        props.getCurrentAnswer(props.answer, props.correctAnswer);
+      }}
+    >
+      <Paper className={props.classes.Paper}>{props.answer}</Paper>
+    </Grid>
+  );
 }
 
 export default withStyles(styles)(QuizAnswer);
