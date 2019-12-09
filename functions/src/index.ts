c44c412c941d947468f6300e7c1bbbe8fe6a1253
Fraser Tooth <frasertooth@gmail.com>
2019-12-09 10:20:54 +0900

    Changed Function Region and CircleCI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,18 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+// For other deployment options
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send(`
-    <h1>QLAB</h1>
-    <p>Hello from all of us at the QLAB Team!</p>
-    <p>Derek 🍆</p>
-    <p>Eriko ☕</p>
-    <p>Fraser 🍻</p>
-    <p>Vic 🌮</p>
-    `);
-});
+export const helloTokyo = functions
+  .region("asia-northeast1")
+  .https.onRequest((request, response) => {
+    response.send(`
+      <h1>QLAB</h1>
+      <p>Hello Tokyo 🗼 from all of us at the QLAB Team!</p>
+      <p>Derek 🍆</p>
+      <p>Eriko ☕</p>
+      <p>Fraser 🍻</p>
+      <p>Vic 🌮</p>
+      `);
+  });
