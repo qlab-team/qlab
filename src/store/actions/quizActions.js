0bf0b02a526dac81b7f0231d079a637c6ba362ca
Derek <derek.forbes77@gmail.com>
2019-12-10 11:42:58 +0900

    taking quizzes updates your score in database, and updates the time you took the test

--- src/store/actions/quizActions.js
+++ src/store/actions/quizActions.js
@@ -18,6 +18,35 @@ const getQuiz = quizId => {
     };
   };
 
+const addQuizInfo = (authId, quizPoints) => {
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+  
+    firestore
+      .collection("users")
+      .where("auth_id", "==", authId)
+      .get()
+      .then(res => {
+        const docId = res.docs[0].id;
+        return docId;
+      })
+      .then(docId => {
+        firestore
+          .collection("users")
+          .doc(docId)
+          .update({
+            q_points: firestore.FieldValue.increment(quizPoints),
+            last_quiz_done: new Date(),
+            quiz_total: firestore.FieldValue.increment(1)
+          })
+          .catch(e => {
+            console.log("err :", e);
+          });
+          console.log('points updated')
+        });
+    }
+}
+
   
 
-export { updateQuizInfo, getQuiz }
+export { addQuizInfo, getQuiz, updateQuizInfo }
