b1060ac9259d17ceb2e38884d113c490a66b7baa
sidiousvic <victor6rene@gmail.com>
2019-12-10 15:11:22 +0900

    Update QuizFinished.jsx

--- src/components/Quiz/QuizFinished.jsx
+++ src/components/Quiz/QuizFinished.jsx
@@ -1,29 +1,62 @@
-import React from 'react'
-import { withStyles, Button, Paper }  from '@material-ui/core'
-import { Link } from "react-router-dom"
-const styles = {
-    Confirmation: {
-      padding: 20,
-      marginTop: 10,
-      marginBottom: 10,
-      height: 60,
-      backgroundColor: 'red',
-      '&:hover': {
-          backgroundColor: 'blue'
-       }
+import React, { useEffect } from "react";
+import { Button, Paper } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import { addQuizInfo } from "../../store/actions/quizActions";
+import { compose } from "redux";
+
+const useStyles = makeStyles(theme => ({
+  Confirmation: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 60,
+    backgroundColor: "red",
+    "&:hover": {
+      backgroundColor: "blue"
     }
-  };
+  }
+}));
+function QuizFinished(props) {
+  const classes = useStyles();
+  console.log(props);
 
-function QuizFinished (props) {
-    return (
-        <React.Fragment>
-            <Paper>Congratulations! You're a god</Paper>
-            <Link color="inherit" to="/dashboard">
-                <Button variant="contained" color="primary">Dashboard</Button>
-            </Link>
+  useEffect(() => {
+    //console.log(props)
+    props.addQuizInfo(props.auth.uid, props.quizPoints);
+  });
 
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Paper>Congratulations! You're a god</Paper>
+      <Link color="inherit" to="/dashboard">
+        <Button
+          className={classes.Confirmation}
+          variant="contained"
+          color="primary"
+        >
+          Dashboard
+        </Button>
+      </Link>
+    </React.Fragment>
+  );
 }
 
-export default withStyles(styles)(QuizFinished);
+const mapStateToProps = state => {
+  return {
+    leaderboard: state.leaderboard,
+    auth: state.firebase.auth
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addQuizInfo: (authId, quizPoints) =>
+      dispatch(addQuizInfo(authId, quizPoints))
+  };
+};
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(
+  QuizFinished
+);
