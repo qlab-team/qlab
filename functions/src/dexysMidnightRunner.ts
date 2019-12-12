6c378f513b27be6a476c23edd3837300bd16650c
Fraser Tooth <frasertooth@gmail.com>
2019-12-12 10:59:52 +0900

    No need for second initialize

--- functions/src/dexysMidnightRunner.ts
+++ functions/src/dexysMidnightRunner.ts
@@ -4,9 +4,6 @@ const functions = require("firebase-functions");
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 
-admin.initializeApp();
-
-
 
 const calculateNewQScore = (user: any) => {
     const now = new Date
