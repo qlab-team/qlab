d98964819806c725ec71d51af629a18c874477ae
sidiousvic <victor6rene@gmail.com>
2019-12-13 14:54:33 +0900

    made dialog logic + component REUSABLE

--- src/components/Utility/Dialog.jsx
+++ src/components/Utility/Dialog.jsx
@@ -6,14 +6,13 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 // actions
-import { purchaseItem } from "../../store/actions/storeActions";
 import { openDialog } from "../../store/actions/dialogActions";
 // redux
 import { connect } from "react-redux";
 import { compose } from "redux";
 
 const AlertDialog = props => {
-  const { auth, user, isDialogOpen, itemData, purchaseError } = props;
+  const { auth, user, isDialogOpen, dialogData, error } = props;
   return (
     <div>
       <Dialog
@@ -25,19 +24,19 @@ const AlertDialog = props => {
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">
-          {!props.purchaseError ? "Do you want to buy this item?" : "Error"}
+          {!error ? "Do you want to buy this item?" : "Error"}
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            {!purchaseError ? (
+            {!error ? (
               <>
-                {itemData.name} for {itemData.price}
+                {dialogData.name} for {dialogData.price}
                 <span style={{ opacity: 0.5, fontSize: "smaller" }}>
                   <sup>ℚ</sup>
                 </span>
               </>
             ) : (
-              purchaseError
+              error
             )}
           </DialogContentText>
         </DialogContent>
@@ -49,27 +48,21 @@ const AlertDialog = props => {
             color="primary"
           >
             <span style={{ fontWeight: "700", color: "white" }}>
-              {!purchaseError && "DISAGREE"}
+              {!error && "DISAGREE"}
             </span>
           </Button>
           <Button
             onClick={() => {
-              props.openDialog(false, null, purchaseError);
-              if (!purchaseError) {
-                const data = {
-                  purchaseDate: new Date().toString(),
-                  itemName: itemData.name,
-                  itemId: itemData.id,
-                  itemPrice: itemData.price
-                };
-                props.purchaseItem(data, auth, user);
+              props.openDialog(false, null, error);
+              if (!error) {
+                props.dialogCallback(props.dialogData, auth, user);
               }
             }}
             color="primary"
             autoFocus
           >
             <span style={{ fontWeight: "700", color: "white" }}>
-              {!purchaseError ? "AGREE" : "GO BACK"}
+              {!error ? "AGREE" : "GO BACK"}
             </span>
           </Button>
         </DialogActions>
@@ -83,16 +76,14 @@ const mapStateToProps = (state, ownProps) => {
   return {
     storeItems: state.store.items,
     isDialogOpen: state.dialog.isDialogOpen,
-    itemData: state.dialog.itemData,
-    purchaseError: state.dialog.purchaseError,
+    dialogData: state.dialog.data,
+    error: state.dialog.error,
     auth: state.firebase.auth,
     user: state.user
   };
 };
 const mapDispatchToProps = dispatch => {
   return {
-    purchaseItem: (data, auth, user) =>
-      dispatch(purchaseItem(data, auth, user)),
     openDialog: (open, data, error) => dispatch(openDialog(open, data, error))
   };
 };
