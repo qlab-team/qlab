312e2152f072aade8514663d438afff7784bed56
nouvelle <nouvellenuts@gmail.com>
2019-12-16 16:44:26 +0900

    Displayed Investors Table

--- src/components/Dashboard/Stats/InvestorsTable.jsx
+++ src/components/Dashboard/Stats/InvestorsTable.jsx
@@ -1,19 +1,14 @@
 /////////////// IMPORTS
 import React from "react";
-import { useEffect } from "react";
 // redux
 import { connect } from "react-redux";
 import { compose } from "redux";
-// actions
-import { getInvestments } from "../../../store/actions/investmentActions";
-// import { openDialog } from "../../../store/actions/dialogActions";
 // components
 import Title from "../Title";
 // material ui
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
-import Button from "@material-ui/core/Button";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
@@ -32,32 +27,19 @@ const useStyles = makeStyles(theme => ({
   seeMore: {
     marginTop: theme.spacing(3)
   },
-  button: {
-    minWidth: theme.spacing(9),
-    borderRadius: 50,
-    fontSize: 10,
-    textTransform: "none",
-    textDecoration: "none !important",
-    padding: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    color: "rgb(92, 27, 249)",
-    transition: "ease-in-out 0.15s",
-    background: "whitesmoke",
-    "&:hover": {
-      background: "rgb(92, 27, 249)",
-      color: "whitesmoke",
-      cursor: "pointer"
-    },
-    boxShadow:
-      "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)"
+  typography: {
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "13px"
+    }
   },
   row: {
     display: "flex",
     alignItems: "center"
   },
   avatar: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
+    width: 20,
+    height: 20
   }
 }));
 
@@ -65,13 +47,7 @@ const useStyles = makeStyles(theme => ({
 const InvestorsTable = props => {
   const classes = useStyles();
   // set props from redux
-  const { auth, investments } = props;
-  useEffect(() => {
-    if (auth.isLoaded) {
-      props.getInvestments(auth);
-    }
-    // eslint-disable-next-line
-  }, [auth.isLoaded]);
+  const { user } = props;
 
   return (
     <React.Fragment>
@@ -84,23 +60,36 @@ const InvestorsTable = props => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {investments.investments.map((investment, i) => (
-            <TableRow key={i}>
-              <TableCell>{investment.date.toDate().toDateString()}</TableCell>
-              <TableCell>
-                {
-                  <Grid container wrap="nowrap" className={classes.row}>
-                    <Avatar
-                      alt="useravatar"
-                      src={investment.photoURL}
-                      className={classes.avatar}
-                    />
-                    <Typography>{investment.display_name}</Typography>
-                  </Grid>
-                }
-              </TableCell>
-            </TableRow>
-          ))}
+          {user.investors
+            ? (user.investors.sort((a, b) => {
+                if (a.timestamp_start < b.timestamp_start) return 1;
+                if (a.timestamp_start > b.timestamp_start) return -1;
+                return 0;
+              }),
+              user.investors.map((investors, i) => (
+                <TableRow key={i}>
+                  <TableCell>
+                    {investors.timestamp_start
+                      .toDate()
+                      .toLocaleDateString("en-US")}
+                  </TableCell>
+                  <TableCell>
+                    {
+                      <Grid container wrap="nowrap" className={classes.row}>
+                        <Typography className={classes.typography}>
+                          {investors.username}
+                        </Typography>
+                        <Avatar
+                          alt="useravatar"
+                          src={investors.photoURL}
+                          className={classes.avatar}
+                        />
+                      </Grid>
+                    }
+                  </TableCell>
+                </TableRow>
+              )))
+            : ""}
         </TableBody>
       </Table>
       <div className={classes.seeMore}>
@@ -115,20 +104,9 @@ const InvestorsTable = props => {
 /////////////// REDUX
 const mapStateToProps = state => {
   return {
-    investments: state.investments,
-    auth: state.firebase.auth,
-    user: state.user,
-    leaderboard: state.leaderboard
-  };
-};
-const mapDispatchToProps = dispatch => {
-  return {
-    getInvestments: auth => dispatch(getInvestments(auth))
-    // openDialog: (open, data) => dispatch(openDialog(open, data))
+    user: state.user
   };
 };
 
 /////////////// EXPORTS
-export default compose(connect(mapStateToProps, mapDispatchToProps))(
-  InvestorsTable
-);
+export default compose(connect(mapStateToProps))(InvestorsTable);
