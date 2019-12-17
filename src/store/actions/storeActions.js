572db3dc1677cdf6b0f3d0dbea89464b377b724e
sidiousvic <victor6rene@gmail.com>
2019-12-17 16:46:01 +0900

    Integrated SET_BADGE_INVISIBLE actions on ListItems and storeActions

--- src/store/actions/storeActions.js
+++ src/store/actions/storeActions.js
@@ -70,6 +70,15 @@ const purchaseItem = (data, auth, user) => {
                 item_price: data.price
               }),
               q_points: user.profile.q_points - data.price
+            })
+            .then(() => {
+              console.log("Show New Badge Called");
+              dispatch({
+                type: "SET_BADGE_INVISIBLE",
+                data: {
+                  profile: false
+                }
+              });
             });
           console.log(
             `Purchase complete. qPoints: ${user.profile.q_points - data.price}`
