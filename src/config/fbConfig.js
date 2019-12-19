1b0772b5ca8101df32cefbb4613205921d49847d
Fraser Tooth <frasertooth@gmail.com>
2019-12-19 11:22:51 +0900

    WIP for Prod

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,8 +2,18 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-// Replace this with your own config details
-const config = {
+const prod_config = {
+  apiKey: "AIzaSyAZ6vZoEKU8R4B4aU3Am2hfJoNDsQ8YrLo",
+  authDomain: "qlab-prod.firebaseapp.com",
+  databaseURL: "https://qlab-prod.firebaseio.com",
+  projectId: "qlab-prod",
+  storageBucket: "qlab-prod.appspot.com",
+  messagingSenderId: "335728755292",
+  appId: "1:335728755292:web:fcbe8ace83a0dc9e4e5c99",
+  measurementId: "G-9J79QHVS3E"
+};
+
+const dev_config = {
   apiKey: "AIzaSyD9aJpocdFwzNf7kVRQk7c0rxfXwcC2og0",
   authDomain: "qlab-cc.firebaseapp.com",
   databaseURL: "https://qlab-cc.firebaseio.com",
@@ -13,7 +23,11 @@ const config = {
   appId: "1:966425750434:web:44c20f5cc3cc3b815620fb",
   measurementId: "G-HDGRZHMZDV"
 };
+
+const config = process.env.APP_VERSION === "PROD" ? prod_config : dev_config;
+// const config = prod_config;
 firebase.initializeApp(config);
+console.log(process.env);
 // firebase.firestore().settings({ timestampsInSnapshots: true });
 firebase.firestore().settings({});
 
