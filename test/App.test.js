6967a1efd1ffb49ba7819b608a6f80e1eb6b560b
nouvelle <nouvellenuts@gmail.com>
2019-12-20 16:41:48 +0900

    Added test: Profile.jsx & Delete test: App.jsx

--- test/App.test.js
+++
@@ -1,8 +0,0 @@
-import App from "../src/components/App";
-
-describe("<App /> rendering", () => {
-  it("should render one <div>", () => {
-    let wrapper = shallow(<App />);
-    expect(wrapper.children("div")).toHaveLength(1);
-  });
-});
