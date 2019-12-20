1a5433975beb2eb107f98b7a4d1ab5326ca4b475
sidiousvic <victor6rene@gmail.com>
2019-12-20 12:27:33 +0900

    Implemented true/false icon in CheckButton

--- src/components/Quiz/AnswerValidator.jsx
+++ src/components/Quiz/AnswerValidator.jsx
@@ -1,24 +1,36 @@
 /////////////// IMPORTS
 import React from "react";
-import { withStyles, Typography } from "@material-ui/core";
-
+import { makeStyles } from "@material-ui/core";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import CancelIcon from "@material-ui/icons/Cancel";
 /////////////// STYLES
-const styles = {
-  Validator: {
-    color: "white",
-    fontSize: 30
+const useStyles = makeStyles(theme => ({
+  true: {
+    fill: "lime",
+    fontSize: 45,
+    marginRight: 10,
+    transition: "linear 1s"
+  },
+  false: {
+    fill: "orangered",
+    fontSize: 45,
+    marginRight: 10,
+    transition: "linear 1s"
   }
-};
+}));
 
 /////////////// COMPONENT
 function AnswerValidator(props) {
+  const classes = useStyles();
   let answerMessage = "";
   if (props.answerConfirmation === "true") answerMessage = "True!";
   else answerMessage = "False!";
-  return (
-    <Typography className={props.classes.Validator}>{answerMessage}</Typography>
+  return answerMessage === "True!" ? (
+    <CheckCircleIcon className={classes.true} />
+  ) : (
+    <CancelIcon className={classes.false} />
   );
 }
 
 /////////////// EXPORTS
-export default withStyles(styles)(AnswerValidator);
+export default AnswerValidator;
